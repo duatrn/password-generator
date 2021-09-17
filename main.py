a43e2f98a5082a9181d6137c8407a8c94c4b26dc
duantrn <duantrn@gmail.com>
2021-09-18 01:40:40 +0700

    minor fix

--- main.py
+++ main.py
@@ -5,46 +5,46 @@ import pyperclip
 from passgen import PasswordGenerator
 
 class App():
-  @classmethod
-  def yes_or_no(cls, questions: str) -> str:
-    """
-    Generate yes or no question and return the choice
-    """
-    while True:
-      yn = input("[" + Fore.GREEN + "?" + Style.RESET_ALL + "] " + questions + ": (y/n) ").lower()
-      if yn != "y" and yn != "n":
-        print(Fore.RED + ">> " + Style.RESET_ALL + "Please enter valid 'y' or 'n'")
-      else:
-        return yn
+    @classmethod
+    def yes_or_no(cls, questions: str) -> str:
+        """
+        Generate yes or no question and return the choice
+        """
+        while True:
+            yn = input("[" + Fore.GREEN + "?" + Style.RESET_ALL + "] " + questions + ": (y/n) ").lower()
+            if yn != "y" and yn != "n":
+                print(Fore.RED + ">> " + Style.RESET_ALL + "Please enter valid 'y' or 'n'")
+            else:
+                return yn
 
-  @click.command()
-  @click.option("--length", "-l", default=8, help="length of passphrase (default: 8)")
-  @click.option("--no-number","-nn", "no_number", is_flag=True, help="has no numbers")
-  @click.option("--no-symbol","-ns", "no_symbol", is_flag=True, help="has no symbols")
-  @click.option("--no-capital","-nc", "no_capital", is_flag=True, help="has no capital letters")
-  def run(length, no_number, no_symbol, no_capital) -> None:
-    pg = PasswordGenerator()
-    if no_number:
-      pg.setConfig(hasNumber=False)
-    if no_symbol:
-      pg.setConfig(hasSymbol=False)
-    if no_capital:
-      pg.setConfig(hasCapital=False)
-    pg.setWordLen(length)
-    passphrase = pg.gen()
-    print("Password generate: "+ Fore.GREEN + f"{passphrase}" + Style.RESET_ALL)
-    
-    # want to copy password just generated to clipboard
-    choice = App.yes_or_no("Save to clipboard")
-    if choice == "y":
-      pyperclip.copy(passphrase)
-      print("[" + Fore.GREEN + "!" + Style.RESET_ALL + "] Password has been saved to clipboard")
-    else:
-      print("[" + Fore.RED + "!" + Style.RESET_ALL + "] Password has not been saved to clipboard")
+    @click.command()
+    @click.option("--length", "-l", default=8, help="length of passphrase (default: 8)")
+    @click.option("--no-number","-nn", "no_number", is_flag=True, help="has no numbers")
+    @click.option("--no-symbol","-ns", "no_symbol", is_flag=True, help="has no symbols")
+    @click.option("--no-capital","-nc", "no_capital", is_flag=True, help="has no capital letters")
+    def run(length, no_number, no_symbol, no_capital) -> None:
+        pg = PasswordGenerator()
+        if no_number:
+            pg.setConfig(hasNumber=False)
+        if no_symbol:
+            pg.setConfig(hasSymbol=False)
+        if no_capital:
+            pg.setConfig(hasCapital=False)
+        pg.setWordLen(length)
+        passphrase = pg.gen()
+        print("Password generate: "+ Fore.GREEN + f"{passphrase}" + Style.RESET_ALL)
+        
+        # want to copy password just generated to clipboard
+        choice = App.yes_or_no("Save to clipboard")
+        if choice == "y":
+            pyperclip.copy(passphrase)
+            print("[" + Fore.GREEN + "!" + Style.RESET_ALL + "] Password has been saved to clipboard")
+        else:
+            print("[" + Fore.RED + "!" + Style.RESET_ALL + "] Password has not been saved to clipboard")
     
 if __name__ == "__main__":
-  try:
-    app = App()
-    app.run()
-  except Exception as err:
-    print(err)
+    try:
+        app = App()
+        app.run()
+    except Exception as err:
+        print(err)
--- passgen.py
+++ passgen.py
@@ -4,98 +4,98 @@ from typing import Dict, Tuple
 from utils import Utils
 
 class PasswordGenerator:
-  def __init__(self) -> None:
-    self._wordLen = 8
-    self._config = {"hasNumber": True, "hasSymbol": True, "hasCapital": True}
-  
-  @property
-  def LETTERS(self) -> Tuple:
-    return (
-      "a", "b", "c", "d", "e", "f", "g", "h", "i" ,"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-    )
-  
-  @property
-  def CAPITALS(self) -> Tuple:
-    return (
-      "A", "B", "C", "D", "E", "F", "G", "H", "I" ,"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    )
+    def __init__(self) -> None:
+        self._wordLen = 8
+        self._config = {"hasNumber": True, "hasSymbol": True, "hasCapital": True}
+    
+    @property
+    def LETTERS(self) -> Tuple:
+        return (
+            "a", "b", "c", "d", "e", "f", "g", "h", "i" ,"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        )
+    
+    @property
+    def CAPITALS(self) -> Tuple:
+        return (
+            "A", "B", "C", "D", "E", "F", "G", "H", "I" ,"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        )
 
-  @property
-  def NUMBERS(self) -> Tuple:
-    return ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    @property
+    def NUMBERS(self) -> Tuple:
+        return ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
-  @property
-  def SYMBOLS(self) -> Tuple:
-    return ("!", "@", "#", "$", "%", "^", "&", "*", ":", "?", "-", "_", "+", "=", "|", "/")
+    @property
+    def SYMBOLS(self) -> Tuple:
+        return ("!", "@", "#", "$", "%", "^", "&", "*", ":", "?", "-", "_", "+", "=")
 
-  @property
-  def wordLen(self) -> int:
-    return self._wordLen
+    @property
+    def wordLen(self) -> int:
+        return self._wordLen
 
-  def setWordLen(self, val: int) -> None:
-    if val < 8:
-      raise ValueError("Error: wordLen must be more than or equal to 8")
-    if not isinstance(val, int):
-      raise ValueError("Error: wordLen must be integer")
-    self._wordLen = val
+    def setWordLen(self, val: int) -> None:
+        if val < 8:
+            raise ValueError("Error: wordLen must be more than or equal to 8")
+        if not isinstance(val, int):
+            raise ValueError("Error: wordLen must be integer")
+        self._wordLen = val
 
-  @property
-  def config(self) -> Dict:
-    return self._config
+    @property
+    def config(self) -> Dict:
+        return self._config
 
-  def setConfig(self, hasNumber: bool = True, hasSymbol: bool = True, hasCapital: bool = True) -> Dict:
-    if not isinstance(hasNumber, bool) or not isinstance(hasSymbol, bool) or not isinstance(hasCapital, bool):
-      raise TypeError("Error: nn, ns, nc must be boolean")
-    if not hasNumber:
-      self._config["hasNumber"] = hasNumber
-    if not hasSymbol:
-      self._config["hasSymbol"] = hasSymbol
-    if not hasCapital:
-      self._config["hasCapital"] = hasCapital
+    def setConfig(self, hasNumber: bool = True, hasSymbol: bool = True, hasCapital: bool = True) -> Dict:
+        if not isinstance(hasNumber, bool) or not isinstance(hasSymbol, bool) or not isinstance(hasCapital, bool):
+            raise TypeError("Error: nn, ns, nc must be boolean")
+        if not hasNumber:
+            self._config["hasNumber"] = hasNumber
+        if not hasSymbol:
+            self._config["hasSymbol"] = hasSymbol
+        if not hasCapital:
+            self._config["hasCapital"] = hasCapital
 
-  def _validate(self, passphrase: str = ""):
-    if passphrase == "": 
-      return False
+    def _validate(self, passphrase: str = ""):
+        if passphrase == "": 
+            return False
 
-    passphrase = tuple([l for l in passphrase]) # convert string to tuple
+        passphrase = tuple([l for l in passphrase]) # convert string to tuple
 
-    if self._config.get("hasNumber") and not Utils._checkItemInIter1InIter2(passphrase, self.NUMBERS): 
-      return False
-    if self._config.get("hasSymbol") and not Utils._checkItemInIter1InIter2(passphrase, self.SYMBOLS): 
-      return False
-    if self._config.get("hasCapital") and not Utils._checkItemInIter1InIter2(passphrase, self.CAPITALS): 
-      return False
+        if self._config.get("hasNumber") and not Utils._checkItemInIter1InIter2(passphrase, self.NUMBERS): 
+            return False
+        if self._config.get("hasSymbol") and not Utils._checkItemInIter1InIter2(passphrase, self.SYMBOLS): 
+            return False
+        if self._config.get("hasCapital") and not Utils._checkItemInIter1InIter2(passphrase, self.CAPITALS): 
+            return False
 
-    return True
+        return True
 
-  def gen(self) -> str:
-    """
-      Generate a passphrase as a string with default configuration
-      - Length 8
-      - At least 1 capital letter
-      - At least 1 symbol
-      - At least 1 number
-    """
+    def gen(self) -> str:
+        """
+            Generate a passphrase as a string with default configuration
+            - Length 8
+            - At least 1 capital letter
+            - At least 1 symbol
+            - At least 1 number
+        """
 
-    # configuration
-    chars = self.LETTERS
-    if self._config.get("hasNumber"): 
-      chars += self.NUMBERS
-    if self._config.get("hasSymbol"): 
-      chars += self.SYMBOLS
-    if self._config.get("hasCapital"): 
-      chars += self.CAPITALS
+        # configuration
+        chars = self.LETTERS
+        if self._config.get("hasNumber"): 
+            chars += self.NUMBERS
+        if self._config.get("hasSymbol"): 
+            chars += self.SYMBOLS
+        if self._config.get("hasCapital"): 
+            chars += self.CAPITALS
 
-    # validation
-    while True:
-      passGen = ""
-      for i in range(0, self._wordLen): 
-        passGen += chars[randint(0, len(chars)-1)]
-      # validate the pass
-      if self._validate(passphrase=passGen): 
-        break
+        # validation
+        while True:
+            passGen = ""
+            for i in range(0, self._wordLen): 
+                passGen += chars[randint(0, len(chars)-1)]
+            # validate the pass
+            if self._validate(passphrase=passGen): 
+                break
 
-    return passGen
+        return passGen
 
 if __name__ == "__main__":
-  pass
+    pass
